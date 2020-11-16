2f11fc99dce770fd174e0a32b1584d2016ad2c18
Jaume Cornadó <jaume@bazingasystems.com>
2020-11-16 16:54:42 +0100

    Iniciamos el SQL Controller

---
+++ GriesserPresuSync/Controllers/MiGriesserSQLController.cs
@@ -0,0 +1,46 @@
+﻿using GriesserPresuSync.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GriesserPresuSync.Controllers
+{
+    class MiGriesserSQLController
+    {
+        public MiGriesserSQLController(GriesserSyncSettings _syncSettings)
+        {
+
+        }
+
+
+
+        string CreateTableSyntax(string TableName)
+        {
+            return @"CREATE TABLE {TableName} (
+	IdLinea Integer PRIMARY KEY,
+	Articulo VARCHAR(255),
+	Cliente VARCHAR(255),
+	NPresupuesto VARCHAR(255),
+	NPersianas Integer,
+	TotalSup FLOAT,
+	TotalAncho FLOAT,
+	TotalLargo FLOAT,
+	LargoTapas FLOAT,
+	TotalTapas Integer,
+	Embalaje VARCHAR(255),
+	POS Integer,
+	BK FLOAT,
+	HL FLOAT,
+	Accion Varchar(255),
+	TL FLOAT,
+	Uni Integer,
+	PUnidad FLOAT,
+	PUnidad2 FLOAT,
+	TEUR FLOAT,
+	POS1 Varchar(255),
+	Color Varchar(255)
+);";
+
+        }
+    }
+}
--- GriesserPresuSync/Models/GriesserSyncSettings.cs
+++ GriesserPresuSync/Models/GriesserSyncSettings.cs
@@ -3,10 +3,15 @@ namespace GriesserPresuSync.Models
 {
     public class GriesserSyncSettings
     {
+        // API Settings
         public string ApiUrl { get; set; }
         public int details = 1;
         public int regs_per_page = 5;
         public int page = 1;
+        // DB Settings
+        public string SageConnectionString { get; set; }
+        public string DestinationTable { get; set; }
+
         public GriesserSyncSettings()
         {
         }
